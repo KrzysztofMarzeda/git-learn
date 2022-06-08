19164386cecfd0b0522f5eb9c4b2397b1cbc0fac
KrzysztofMarzeda <xeventris@gmail.com>
2022-06-08 19:43:28 +0200

    Html and css changes for homepage and browse

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
@@ -27,8 +28,12 @@ class VinylController extends AbstractController
         ]);
     }
     #[Route('/browse/{slug}')]
-    public function browse($slug = null): Response
+    public function browse(string $slug = null): Response
     {
-        return new Response('działa browse');
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig',[
+            'genre' => $genre,
+        ]);
     }
 }
